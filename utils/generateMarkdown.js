d895547ec2bb26676e183132530e8904a27e2199
Oscar Medina <ogmedina04@gmail.com>
2020-12-14 20:58:11 -0600

    finished most of the coding for readme creation, need to work on license plates

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,42 @@ function generateMarkdown(data) {
   return `
   # ${data.project}
 
-`;
-}
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  To install necessary dependencies, run the following command:
+
+  ${data.installdep}
+
+  ## Usage 
+  ${data.usingrepo}
+
+  ## License
+  This project is licensed under the ${data.license} License.
+
+  ## Contributing
+  ${data.contrepo}
+
+  ## Tests
+  To run tests, run the following command:
+
+  ${data.testdep}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You 
+  can find more of my work at [${data.githubuser}](https://github.com/${data.githubuser}/)
+  
+
+`};
 
 module.exports = generateMarkdown;
